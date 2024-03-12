12e66470838b25c3f2dc7238ef4b5cf568cf6653
arifal <43651600+arifal201@users.noreply.github.com>
2024-03-12 16:29:11 +0700

    fundamental return

---
+++ fundamenta_return.py
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/55a5befdf16499bffb00007b
+def add(a,b):
+  return a + b 
+def multiply(a,b):
+  return a * b
+def divide(a,b):
+  return a / b
+def mod(a,b):
+  return a % b
+def exponent(a,b):
+  return a ** b
+def subt(a,b):
+  return a - b
+  
+print(exponent(95,49))
